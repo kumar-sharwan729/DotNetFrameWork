3749cfac6d4876d8431a746313aac075ec8aeec8
Sharwan Kumar <81203489+kumar-sharwan729@users.noreply.github.com>
2022-02-20 08:46:00 +0530

    ReadKeyDemo_C#
    Get a mask key value with the help of ReadKey

---
+++ ReadKeyDemo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDemoApp.ConsoleKeyFolder
+{
+    internal class ReadKeyDemo
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the password");
+            ConsoleKeyInfo ck = Console.ReadKey(true);
+            string password = "";
+
+            while (ck.Key != ConsoleKey.Enter)
+            {
+                
+                Console.Write("#");
+                password += ck.KeyChar;
+                ck = Console.ReadKey(true);
+            }
+            Console.WriteLine("Entered Password is "+ password);
+        }
+    }
+}
